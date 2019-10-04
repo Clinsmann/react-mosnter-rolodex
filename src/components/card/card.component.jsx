5e136ed93b494f343429b42e89b9c9ca0094c274
Hillary.Ibeanu <hillary.ibeanu@interswitchng.com>
2019-10-04 02:45:43 +0100

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card.styles.css";
+
+export const Card = props => (
+	<div className="card-container">
+		<img
+			src={`https://robohash.org/${props.monster.id}?set=set5`}
+			width="220"
+			alt={`monster ${props.monster.id}`}
+		/>
+		<h3>{props.monster.name}</h3>
+		<p>{props.monster.email}</p>
+	</div>
+);
---
+++ src/components/search-box/search-box..component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export const SearchBox = ({ handleChange }) => (
+	<input className="search" type="search" placeholder="search monsters" onChange={handleChange} />
+);
+ 
