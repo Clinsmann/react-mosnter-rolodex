02bddf6d9c2d754225877a8ca5a1f91ec8f7b805
Hillary.Ibeanu <hillary.ibeanu@interswitchng.com>
2019-10-04 02:42:42 +0100

    Updates

---
+++ precache-manifest.e57eef69bf7e2f6d7927e0a813e051b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b57a943feaa6f5e8dcb4370036f14a",
+    "url": "/react-mosnter-rolodex/index.html"
+  },
+  {
+    "revision": "95c04004bbc055abdeac",
+    "url": "/react-mosnter-rolodex/static/css/main.edccab91.chunk.css"
+  },
+  {
+    "revision": "c9b7a721d8a55e7a5050",
+    "url": "/react-mosnter-rolodex/static/js/2.fb04d3fc.chunk.js"
+  },
+  {
+    "revision": "95c04004bbc055abdeac",
+    "url": "/react-mosnter-rolodex/static/js/main.10ac304f.chunk.js"
+  },
+  {
+    "revision": "cdee82f5b612cb795c06",
+    "url": "/react-mosnter-rolodex/static/js/runtime-main.b3f0c1a4.js"
+  }
+]);
